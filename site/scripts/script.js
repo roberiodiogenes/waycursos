c11e3b39a214f12d96b63b70f612cdc2c24e30ba
Robério Diógenes <96064387+roberiodiogenes@users.noreply.github.com>
2023-04-25 12:54:23 -0300

    inicio
    Início do repositório da Way Cursos.
    25 de abril de 2023.

---
+++ site/scripts/script.js
@@ -0,0 +1,7 @@
+let instagram = "https://www.instagram.com/wayqualificacoes/";
+let facebook  = "https://www.facebook.com/wayqualificacoes/?locale=pt_BR";
+let tiktok    = "#";
+let gps       = "https://goo.gl/maps/33FPTSuTQkM3ZNc9A";
+let email     = "wayqualicacoes@gmail.com";
+let whatsapp  = "https://api.whatsapp.com/send?phone=5585987054934";
+let celular   = "5585987054934";
